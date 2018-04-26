f9823e35b5a639ed06a719b592bfd9e72aaf075c
mzedlach <mariazlach@gmail.com>
2018-04-26 13:20:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+//Lecture: Variables
+/*
+var name = 'Maria';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26; 
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+//Lecture: Variables 2
+var name = 'Maria';
+var age = 26; 
+
+console.log(name + age);
