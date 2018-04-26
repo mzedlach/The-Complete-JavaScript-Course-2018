5085c62f061bc1c73dfdd18e12d80027cb1c163d
mzedlach <mariazlach@gmail.com>
2018-04-26 13:38:05 +0200

    Lecture 2 - Variables 2

--- script.js
+++ script.js
@@ -19,4 +19,23 @@ console.log(fullAge);
 var name = 'Maria';
 var age = 26; 
 
-console.log(name + age);
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried; 
+//console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
