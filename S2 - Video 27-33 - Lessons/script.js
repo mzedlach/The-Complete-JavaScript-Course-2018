f4205bc45aade20fd2836110c8c091181ba13589
mzedlach <mariazlach@gmail.com>
2018-04-28 17:16:56 +0200

    Scoping

--- S2 - Video 27-33 - Lessons/script.js
+++ S2 - Video 27-33 - Lessons/script.js
@@ -1,7 +1,7 @@
 /*------------------*/
 /*-----Hoisting-----*/
 /*------------------*/
-
+/*
 //Functions
 calculateAge(1990);
 
@@ -27,4 +27,44 @@ function foo() {
 }
 foo();
 console.log(age);
+*/
+
+/*-----------------*/
+/*-----Scoping-----*/
+/*-----------------*/
+
+// First scoping example
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+
+
+// Example to show the differece between execution stack and scope chain
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    //console.log(c);
+    console.log(a+d);
+}
 
