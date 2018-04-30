329e2c1be533d0fc04de02c8436e17fb7606cd0d
mzedlach <mariazlach@gmail.com>
2018-04-30 16:36:20 +0200

    Events and Event Handlers: Folling the Dice + Updating Scores and Changing the Active Player

--- S4 - Video 34-44 - Lessons/app.js
+++ S4 - Video 34-44 - Lessons/app.js
@@ -9,33 +9,78 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6) +1 ;
+//Make dice invisible when page loads
+document.querySelector('.dice').style.display = 'none'; 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+//    1. Random Number
+    var dice = Math.floor(Math.random()*6) +1 ;
+//    2. Display the XPathResult
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'; 
+    
+//    3. Update the round score IF the rolled number is NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+//        Both 'current' points will be set to zero
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+//        document.querySelector('.player-0-panel').classList.remove('active');
+//        document.querySelector('.player-1-panel').classList.add('active');
+        
+//        Active player will switch. Other player gets grey background and red dot
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+//        Dice will disappear
+        document.querySelector('.dice').style.display = 'none';
+    }
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//var x = document.querySelector('#score-0').textContent;
 
 
 
