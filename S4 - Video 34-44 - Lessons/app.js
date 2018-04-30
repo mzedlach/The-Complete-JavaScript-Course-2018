e9ae4109ed929e72b0983481c62d1c977d614809
mzedlach <mariazlach@gmail.com>
2018-04-30 16:57:06 +0200

    Hold and Winning the Game

--- S4 - Video 34-44 - Lessons/app.js
+++ S4 - Video 34-44 - Lessons/app.js
@@ -39,9 +39,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next Player
+        nextPlayer();
+    }
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+//    1. Add current score to global score
+    scores[activePlayer] += roundScore;
+//    2. Update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+//    3. Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+//    Next Player
+    nextPlayer();
+    }
+
+    
+});
+
+function nextPlayer() {
+//Next Player
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
-        
 //        Both 'current' points will be set to zero
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
@@ -54,11 +79,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 //        Dice will disappear
         document.querySelector('.dice').style.display = 'none';
-    }
-    
-});
-
-
+}
 
 
 
