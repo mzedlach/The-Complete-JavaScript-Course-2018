5b59819edee7f605e8e6e4846d2d90418db6e178
mzedlach <mariazlach@gmail.com>
2018-04-30 17:08:15 +0200

    Game Initialization

--- S4 - Video 34-44 - Lessons/app.js
+++ S4 - Video 34-44 - Lessons/app.js
@@ -11,18 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-//Make dice invisible when page loads
-document.querySelector('.dice').style.display = 'none'; 
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init(); 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 //    1. Random Number
@@ -59,8 +48,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 //    Next Player
     nextPlayer();
     }
-
-    
 });
 
 function nextPlayer() {
@@ -81,10 +68,34 @@ function nextPlayer() {
         document.querySelector('.dice').style.display = 'none';
 }
 
+//When the new game is clicked
+document.querySelector('.btn-new').addEventListener('click', init);
 
 
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    
+    //Make dice invisible when page loads
+    document.querySelector('.dice').style.display = 'none'; 
 
-
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+}
 
 
 
