42363d027855ecf6ce8b8f66784a0245be405ef3
mzedlach <mariazlach@gmail.com>
2018-05-03 16:32:54 +0200

    Functions Returning Functions

--- S5 - Video 49-60 - Lessons/script.js
+++ S5 - Video 49-60 - Lessons/script.js
@@ -103,7 +103,7 @@ console.log(obj.city);
 /*------------------------------------*/
 //---Passing functions as arguments---//
 /*------------------------------------*/
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -139,10 +139,38 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
 
+/*-----------------------------------*/
+//---Functions Returning Functions---//
+/*-----------------------------------*/
 
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?')
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+};
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
 
 
 
