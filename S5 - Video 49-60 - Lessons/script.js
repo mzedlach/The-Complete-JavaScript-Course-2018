a622aacc705df663d6c0aec121c53ffb4e9a6eff
mzedlach <mariazlach@gmail.com>
2018-05-03 21:56:08 +0200

    Bind, Call, and Apply

--- S5 - Video 49-60 - Lessons/script.js
+++ S5 - Video 49-60 - Lessons/script.js
@@ -211,7 +211,7 @@ interviewQuestion('teacher')('Mark');
 /*--------------*/
 //---Closures---//
 /*--------------*/
-
+/*
 function retirement(retirementAge) {
     var a = ' years left until retirement';
     return function(yearOfBirth) {
@@ -248,22 +248,85 @@ function interviewQuestion(job) {
 interviewQuestion('teacher')('Mark');
 interviewQuestion('designer')('John');
 interviewQuestion('')('Jane');
+*/
+
+/*---------------------------*/
+//---Bind, Call, and Apply---//
+/*---------------------------*/
+
+var john = {
+    name: 'John', 
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m ' + 
+                        this.name + '. I\'m a ' + 
+                        this.job + ' and I\'m ' + 
+                        this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + 
+                        this.name + '. I\'m a ' + 
+                        this.job + ' and I\'m ' + 
+                        this.age + ' years old. Have a nice ' +
+                       timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily', 
+    age: 35,
+    job: 'designer',
+}
+
+
+john.presentation('friendly', 'morning'); 
+
+//This is called Method Borrowing
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//The following will not work because our function does expect an array
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
 
 
 
 
 
 
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    
+    for (var i = 0 ; i < arr.length ; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
 
 
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 
-
-
-
-
-
-
-
+console.log(ages);
+console.log(fullJapan);
 
 
 
