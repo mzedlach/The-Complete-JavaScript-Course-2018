06756dcce5258487c7ba58d63c7c4a4928607601
mzedlach <mariazlach@gmail.com>
2018-05-07 10:49:21 +0200

    Creating Income and Expense Function Constructors

--- S6 - Videos 66-91/app.js
+++ S6 - Videos 66-91/app.js
@@ -1,7 +1,35 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
     
-    //Some Code
+    
+//Function constructor for created expenses (private function)
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+//Function constructor for created expenses (private function)
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+//Variables holding data
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0, 
+            inc: 0
+        }
+    }
+    
+    
+    
     
 })();
 
