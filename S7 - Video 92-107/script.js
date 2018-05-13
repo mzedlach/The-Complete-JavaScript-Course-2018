813b00986fd50e977c431343a2cd6f8fe6af01d2
mzedlach <mariazlach@gmail.com>
2018-05-13 18:39:13 +0200

    Default Parameters

--- S7 - Video 92-107/script.js
+++ S7 - Video 92-107/script.js
@@ -409,7 +409,7 @@ function isFullAge6(...years) {
 //isFullAge6(1990, 1999, 1965);
 isFullAge6(1990, 1999, 1965, 2016, 1987);
 */
-
+/*
 // ES5
 function isFullAge5(limit) {
     // The added '1' says to start cutting at arguemnt #1
@@ -430,8 +430,36 @@ function isFullAge6(limit, ...years) {
 
 
 isFullAge6(16,1990, 1999, 1965, 2016, 1987);
+*/
+
+/*----------------------------*/
+/*-----Default parameters-----*/
+/*----------------------------*/
+
+// ES5
+
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+    
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'american' : nationality = nationality; 
+    
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+// ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
 
 
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 
 
 
