460e1f71bf6f95c31eac2c0d83bece5ae83b1855
mzedlach <mariazlach@gmail.com>
2018-05-13 14:55:28 +0200

    Destructuring

--- S7 - Video 92-107/script.js
+++ S7 - Video 92-107/script.js
@@ -206,7 +206,7 @@ const box66 = {
 box66.clickMe();
 //this becomes undefined again
 */
-
+/*
 function Person(name) {
     this.name = name;
 }
@@ -229,6 +229,72 @@ Person.prototype.myFriends6 = function(friends) {
 }
 
 new Person('Mike').myFriends6(friends);
+*/
+
+/*-----------------------*/
+/*-----Destructuring-----*/
+/*-----------------------*/
+
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+//var age = john[1];
+
+// ES6
+
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+
+const obj = {
+    firstName: 'John', 
+    lastName: 'Smith'
+}
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+// ES5
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear()- year; 
+    return [age, 65 - age]
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
