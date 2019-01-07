b45b6cc1961285c1b369bbd51451c8e05fe33946
mzedlach <mariazlach@gmail.com>
2019-01-07 20:22:22 +0100

    Building the Search Model

--- S9-Video-120-148-Forkify-Project/src/js/index.js
+++ S9-Video-120-148-Forkify-Project/src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = 'cc0ab89c8d7d6c05b01c6ab5c1a43102'
-  try {
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-  
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- S9-Video-120-148-Forkify-Project/src/js/models/Search.js
+++ S9-Video-120-148-Forkify-Project/src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an exported string.';
+
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'cc0ab89c8d7d6c05b01c6ab5c1a43102'
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
