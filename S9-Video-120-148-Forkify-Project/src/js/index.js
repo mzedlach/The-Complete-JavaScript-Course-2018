ec71d804312a4ea9ba727d527fee1bd27189adf9
mzedlach <mariazlach@gmail.com>
2019-01-07 16:15:49 +0100

    Three ways to import ES6 Modules

--- S9-Video-120-148-Forkify-Project/src/js/index.js
+++ S9-Video-120-148-Forkify-Project/src/js/index.js
@@ -1,5 +1,9 @@
-// Global app controller
-import num from './test';
-const x = 23 ; 
+import str from './models/Search' ;
+// import { add, multiply, id } from './views/searchView' ; 
+// console.log(`Using imported functions! ${add(id, 2)} and ${multiply(3, 5)}, ${str}`);
 
-console.log(`I imported ${num} from another module called test.js Variable x is ${x}`);
+// import { add as a, multiply as m , id } from './views/searchView' ; 
+// console.log(`Using imported functions! ${a(id, 2)} and ${m(3, 5)}, ${str}`);
+
+import * as searchView from './views/searchView';
+console.log(`Using imported functions! ${searchView.add(searchView.id, 2)} and ${searchView.multiply(3, 5)}, ${str}`);
