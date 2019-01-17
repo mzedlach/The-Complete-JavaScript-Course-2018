d0c00534806ed3ee76a5e1ca4a610e6d5a78eb8a
mzedlach <mariazlach@gmail.com>
2019-01-17 12:16:20 +0100

    Generating laptop & overview pages

--- S10-Video-162-164-Bonus-NodeJS Crash course/index.js
+++ S10-Video-162-164-Bonus-NodeJS Crash course/index.js
@@ -12,14 +12,44 @@ const server = http.createServer((req, res) => {
 
   console.log(id);
 
+  // PRODUCTS OVERVIEW 
   if (pathName === '/products' || pathName === '/') {
     res.writeHead(200, { 'Content-type': 'text-html'});
-    res.end('This is the PRODUCTS page');
+    
+    fs.readFile(`${__dirname}/templates/template-overview.html` ,'utf-8', (err, data) => {
+      let overviewOutput = data; 
+
+      fs.readFile(`${__dirname}/templates/template-card.html` ,'utf-8', (err, data) => {
+
+
+        const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+        overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+        res.end(overviewOutput);
+      });
+
+    });
+
   } 
+  // LAPTOP DETAIL
   else if  (pathName === '/laptop' && id < laptopData.length) {
     res.writeHead(200, { 'Content-type': 'text-html'});
-    res.end(`This is the LAPTOP page for laptop ${id}!`);
+    
+    fs.readFile(`${__dirname}/templates/template-laptop.html` ,'utf-8', (err, data) => {
+      const laptop = laptopData[id];
+      const output = replaceTemplate(data, laptop);
+      res.end(output);
+    });
   } 
+
+  // IMAGES ROUTE 
+  else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+    fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+      res.writeHead(200, { 'Content-type': 'img/jpg'});
+      res.end(data);
+    })
+  }
+
+  // URL NO FOUND
   else {
     res.writeHead(404, { 'Content-type': 'text-html'});
     res.end('URL not found on server. Womp womp. ');
@@ -32,5 +62,18 @@ server.listen(1337, '127.0.0.1', () => {
   console.log('Server has started listening. Listening for requests now.');
 });
 
+function replaceTemplate(originalHTML, laptop) {
+  let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+
+  return output;
+}
 
 // To run this, write nodemon in terminal
